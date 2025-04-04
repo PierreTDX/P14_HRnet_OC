fce8c97279599f11c3c772a48769b84d796184db
PierreTDX <pierre.tondeux@gmail.com>
2025-04-04 11:12:43 +0200

    ne pas recharger la liste si déjà chragée une première fois

--- loadEmployees.js
+++ loadEmployees.js
@@ -1,8 +1,15 @@
-fetch('generated_users.json')
-  .then(response => response.json())
-  .then(employees => {
-    localStorage.setItem('employees', JSON.stringify(employees));
-  })
-  .catch(error => {
-    console.error("Erreur lors du chargement des employés :", error);
-  });
+if (!localStorage.getItem('employees')) {
+  fetch('generated_users.json')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`Erreur HTTP : ${response.status}`);
+      }
+      return response.json();
+    })
+    .then(employees => {
+      localStorage.setItem('employees', JSON.stringify(employees));
+    })
+    .catch(error => {
+      console.error("Erreur lors du chargement des employés :", error);
+    });
+};
