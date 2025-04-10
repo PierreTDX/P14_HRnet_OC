e2b2f1f52ff9ff3b7eb09564cfed823394cde71c
PierreTDX <pierre.tondeux@gmail.com>
2025-04-10 20:10:17 +0200

    recherche dept et state

--- src/pages/ListEmployees/index.jsx
+++ src/pages/ListEmployees/index.jsx
@@ -15,11 +15,11 @@ createTheme(
 const ListEmployees = () => {
     const [employees, setEmployees] = useState([]);
     const [search, setSearch] = useState('');
-    const [columns, setColumns] = useState([
-        { name: 'First Name', selector: row => row.firstName, sortable: true },
-        { name: 'Last Name', selector: row => row.lastName, sortable: true },
-    ]);
+    const [columns, setColumns] = useState([]);
     const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+    const [stateFilter, setStateFilter] = useState('');
+    const [departementFilter, setDepartementFilter] = useState('');
+
 
     useEffect(() => {
         // Récupérer les données depuis le localStorage
@@ -40,12 +40,21 @@ const ListEmployees = () => {
         };
     }, []);
 
-    // Filtrer les données en fonction de la recherche
-    const filteredData = employees.filter(item =>
-        Object.values(item).some(value =>
-            value.toString().toLowerCase().includes(search.toLowerCase())
-        )
-    );
+    // Fonction pour filtrer les données en fonction de la recherche
+    const filteredData = employees.filter(item => {
+        const searchTerms = search.toLowerCase().split(' ');
+
+        const matchesSearch = searchTerms.every(term =>
+            Object.values(item).some(value =>
+                value.toString().toLowerCase().includes(term)
+            )
+        );
+
+        const matchesState = item.state.toLowerCase().includes(stateFilter.toLowerCase());
+        const matchesDep = item.department.toLowerCase().includes(departementFilter.toLowerCase());
+
+        return matchesSearch && matchesState && matchesDep;
+    });
 
     // Mettre à jour les colonnes affichées en fonction de la largeur de l'écran
     useEffect(() => {
@@ -59,14 +68,44 @@ const ListEmployees = () => {
                 { name: 'First Name', selector: row => row.firstName, sortable: true },
                 { name: 'Last Name', selector: row => row.lastName, sortable: true },
                 { name: 'Start Date', selector: row => new Date(row.startDate), sortable: true, format: row => row.startDate },
-                { name: 'Department', selector: row => row.department, sortable: true }
+                {
+                    name:
+                        (
+                            <div className="state-header">
+                                Department
+                                <input
+                                    type="text"
+                                    placeholder="Filter"
+                                    onChange={(e) => setDepartementFilter(e.target.value)}
+                                    className="state-filter-input"
+                                />
+                            </div>
+                        ),
+
+                    selector: row => row.department, sortable: true
+                }
             ]);
         } else if (windowWidth > 768 && windowWidth <= 1024) {
             setColumns([
                 { name: 'First Name', selector: row => row.firstName, sortable: true },
                 { name: 'Last Name', selector: row => row.lastName, sortable: true },
                 { name: 'Start Date', selector: row => new Date(row.startDate), sortable: true, format: row => row.startDate },
-                { name: 'Department', selector: row => row.department, sortable: true },
+                {
+                    name:
+                        (
+                            <div className="state-header">
+                                Department
+                                <input
+                                    type="text"
+                                    placeholder="Filter"
+                                    onChange={(e) => setDepartementFilter(e.target.value)}
+                                    className="state-filter-input"
+                                />
+                            </div>
+                        ),
+
+                    selector: row => row.department, sortable: true
+                },
                 { name: 'Street', selector: row => row.street, sortable: true },
                 { name: 'City', selector: row => row.city, sortable: true }
             ]);
@@ -75,11 +114,42 @@ const ListEmployees = () => {
                 { name: 'First Name', selector: row => row.firstName, sortable: true },
                 { name: 'Last Name', selector: row => row.lastName, sortable: true },
                 { name: 'Start Date', selector: row => new Date(row.startDate), sortable: true, format: row => row.startDate },
-                { name: 'Department', selector: row => row.department, sortable: true },
+                {
+                    name:
+                        (
+                            <div className="state-header">
+                                Department
+                                <input
+                                    type="text"
+                                    placeholder="Filter"
+                                    onChange={(e) => setDepartementFilter(e.target.value)}
+                                    className="state-filter-input"
+                                />
+                            </div>
+                        ),
+
+                    selector: row => row.department, sortable: true
+                },
                 { name: 'Date of Birth', selector: row => new Date(row.dateOfBirth), sortable: true, format: row => row.dateOfBirth },
                 { name: 'Street', selector: row => row.street, sortable: true },
                 { name: 'City', selector: row => row.city, sortable: true },
-                { name: 'State', selector: row => row.state, sortable: true, width: '90px' },
+                {
+                    name:
+                        (
+                            <div className="state-header">
+                                State
+                                <input
+                                    type="text"
+                                    placeholder="Filter"
+                                    onChange={(e) => setStateFilter(e.target.value)}
+                                    className="state-filter-input"
+                                />
+                            </div>
+                        ),
+                    selector: row => row.state,
+                    sortable: true,
+                    width: '90px'
+                },
                 { name: 'Zip Code', selector: row => row.zipCode, sortable: true, width: '118px' }
             ]);
         }
@@ -102,7 +172,7 @@ const ListEmployees = () => {
                         className="search-input"
                     />
                     <div className="pagination-info">
-                        <p>{filteredData.length} out of {employees.length} results</p>
+                        <p>{filteredData.length}/{employees.length} results</p>
                     </div>
                 </div>
                 <div className='tableContent'>
@@ -114,7 +184,6 @@ const ListEmployees = () => {
                         pointerOnHover
                         striped
                         ariaLabel
-                        // paginationTotalRows={filteredData.length}
                         theme="custom"
                     />
                 </div>
