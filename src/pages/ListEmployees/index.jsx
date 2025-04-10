dd388548a0832b8285975827c221bba18299aa99
PierreTDX <pierre.tondeux@gmail.com>
2025-04-10 16:09:50 +0200

    custom tableau de la liste d'emlpoyés

--- src/pages/ListEmployees/index.jsx
+++ src/pages/ListEmployees/index.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import DataTable from 'react-data-table-component';
+import DataTable, { createTheme } from 'react-data-table-component';
 import './listEmployees.scss';
 
 // Fonction pour convertir les dates en format ISO
@@ -8,16 +8,46 @@ const formatDate = (dateStr) => {
     return new Date(`${year}-${month}-${day}`).toISOString();
 };
 
+createTheme(
+    'custom',
+    {
+        divider: {
+            default: '#073642',
+        },
+    }
+);
+
 const columns = [
     { name: 'First Name', selector: row => row.firstName, sortable: true },
     { name: 'Last Name', selector: row => row.lastName, sortable: true },
-    { name: 'Start Date', selector: row => row.startDate, sortable: true, format: row => new Date(formatDate(row.startDate)).toLocaleDateString() },
+    {
+        name: 'Start Date',
+        selector: row => new Date(formatDate(row.startDate)), // pour le tri
+        sortable: true,
+        format: row => row.startDate // pour l'affichage
+    },
     { name: 'Department', selector: row => row.department, sortable: true },
-    { name: 'Date of Birth', selector: row => row.dateOfBirth, sortable: true, format: row => new Date(formatDate(row.dateOfBirth)).toLocaleDateString() },
+    {
+        name: 'Date of Birth',
+        selector: row => new Date(formatDate(row.dateOfBirth)),
+        sortable: true,
+        format: row => row.dateOfBirth
+    },
     { name: 'Street', selector: row => row.street, sortable: true },
     { name: 'City', selector: row => row.city, sortable: true },
-    { name: 'State', selector: row => row.state, sortable: true },
-    { name: 'Zip Code', selector: row => row.zipCode, sortable: true },
+    {
+        name: 'State',
+        selector: row => row.state,
+        sortable: true,
+        width: '90px'
+    },
+    {
+        name: 'Zip Code',
+        selector: row => row.zipCode,
+        sortable: true,
+        width: '118px'
+
+    }
 ];
 
 const ListEmployees = () => {
@@ -42,26 +72,32 @@ const ListEmployees = () => {
     );
 
     return (
-        <div>
+        <>
             <h1 className='pageTitle'>Current Employees</h1>
-            <div className="search-container">
-                <input
-                    type="text"
-                    placeholder="Search all columns"
-                    value={search}
-                    onChange={handleSearch}
-                    className="search-input"
-                />
-            </div>
-
-            <DataTable
-                columns={columns}
-                data={filteredData}
-                pagination
-                highlightOnHover
-                striped
-            />
-        </div>
+            <main>
+                <div className="search-container">
+                    <input
+                        type="text"
+                        placeholder="Search all columns"
+                        value={search}
+                        onChange={handleSearch}
+                        className="search-input"
+                    />
+                </div>
+                <div className='tableContent'>
+                    <DataTable
+                        className='customTable'
+                        columns={columns}
+                        data={filteredData}
+                        pagination
+                        pointerOnHover
+                        striped
+                        ariaLabel
+                        theme="custom"
+                    />
+                </div>
+            </main>
+        </>
     );
 };
 
